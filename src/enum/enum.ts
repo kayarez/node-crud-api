3660e67fd0467b801d4358debffd1eacca500b14
kayarez <tansteppp@gmail.com>
2022-06-18 21:20:05 +0300

    feat: add messages enum

---
+++ src/enum/enum.ts
@@ -0,0 +1,13 @@
+export enum Errors {
+    USERNAME_NOT_FOUND = "Please, check username!",
+    AGE_NOT_FOUND = "Please, check age!",
+    HOBBIES_NOT_FOUND = "Please, check hobbies!",
+    USER_ID_NOT_FOUND = "Please, check userId!",
+    USER_NOT_FOUND = "User not found!",
+    BODY_NOT_FOUND = "Please, check body data!",
+
+    UUID_NOT_VALID = "UUID is not valid!",
+
+    INVALID_HTTP_METHOD = "Invalid http method!",
+    INVALID_URL = "Invalid url!",
+}
